0edc3dda4b74216bb78a2a39dd21ba7d9b6d971b
yayanyang <yayanyang@gmail.com>
2015-11-17 10:56:52 +0800

    update test

--- cassandra/cassandra_test.go
+++ cassandra/cassandra_test.go
@@ -1,26 +1,88 @@
 package cassandra
 
 import (
+	"testing"
 	"time"
 
 	"github.com/gschat/gocql"
 )
 
-var session *gocql.Session
+var cluster = gocql.NewCluster("192.168.88.2:7000", "192.168.88.3:7000", "192.168.88.4:7000")
 
-// init the cassandra test env
-func init() {
-	cluster := gocql.NewCluster("192.168.88.2:7000", "192.168.88.3:7000", "192.168.88.4:7000")
-
-	cluster.Keyspace = "bench"
+func TestCreateKeySpace(t *testing.T) {
+	cluster.Keyspace = "system"
 
 	cluster.Timeout = 5 * time.Second
 
 	var err error
 
-	session, err = cluster.CreateSession()
+	session, err := cluster.CreateSession()
 
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer session.Close()
+
+	err = session.Query(`DROP KEYSPACE IF EXISTS bench`).Exec()
 	if err != nil {
 		panic(err)
 	}
+
+	err = session.Query(`CREATE KEYSPACE bench
+	WITH replication = {
+		'class' : 'SimpleStrategy',
+		'replication_factor' :
+	}`).Exec()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func createSession() *gocql.Session {
+	cluster.Keyspace = "bench"
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		panic(err)
+	}
+
+	return session
+}
+
+func TestCreateTable(t *testing.T) {
+	session := createSession()
+	defer session.Close()
+
+	if err := session.Query(
+		`CREATE TABLE bench.SQID_TABLE(
+			name varchar primary key,
+			id   int
+		 )`,
+	).Exec(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func BenchmarkUpdate(b *testing.B) {
+	b.StopTimer()
+	session := createSession()
+
+	if err := session.Query(`INSERT INTO bench.SQID_TABLE (name,id)
+		VALUES ("test",0) IF NOT EXISTS`).Exec(); err != nil {
+		b.Fatal("insert:", err)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		if err := session.Query(`UPDATE bench.SQID_TABLE SET id=? WHERE name=test`, i).Exec(); err != nil {
+			b.Fatal("insert:", err)
+		}
+	}
+
+	b.StopTimer()
+	session.Close()
+	b.StartTimer()
 }
